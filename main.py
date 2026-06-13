57b45c44aabb211c540e749a660da70c06099f0c
KarolinaGutkowska <karolina.gutkowska4@onet.pl>
2026-06-13 21:42:10 +0200

    Dodano folder dla HUBA

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 # This is a sample Python script.
 
-print(" Hello world")
+from HUBA.huba import HUBA
+
+huba = HUBA()
+
+clean_file, report = huba.run(
+    input_file="dane.xlsx",
+    output_file="dane_clean.xlsx"
+)
